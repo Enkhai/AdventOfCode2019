d402ac8c6746a61a006ec74e98ee23bf5e7dce0b
Enkhai <aerogtx@gmail.com>
2019-12-12 17:45:05 +0200

    Fixes

--- advent2.py
+++ advent2.py
@@ -1,30 +1,30 @@
 import sys
 
-input = []
+input_list = []
 with open("inputs/advent2input.txt", "r") as file:
     for line in file.readlines():
-        input += line.split(",")
-input = [int(x) for x in input]
+        input_list += line.split(",")
+input_list = [int(x) for x in input_list]
 
 
-def operation(type, in1, in2, target):
-    if type == 1:
-        input[target] = input[in1] + input[in2]
-    elif type == 2:
-        input[target] = input[in1] * input[in2]
-    elif type == 99:
+def operation(op_type, in1, in2, target):
+    if op_type == 1:
+        input_list[target] = input_list[in1] + input_list[in2]
+    elif op_type == 2:
+        input_list[target] = input_list[in1] * input_list[in2]
+    elif op_type == 99:
         print("Operation type 99. Halting program.")
-        print("Value at position zero is: %d" % input[0])
+        print("Value at position zero is: %d" % input_list[0])
         sys.exit()
     else:
         print("Wrong operation type!")
         sys.exit()
 
-    print("Target value (%d): %d" %(target, input[target]))
+    print("Target value (%d): %d" % (target, input_list[target]))
 
 
 if __name__ == '__main__':
     i = 0
-    while i < len(input):
-        operation(input[i], input[i + 1], input[i + 2], input[i + 3])
+    while i < len(input_list):
+        operation(input_list[i], input_list[i + 1], input_list[i + 2], input_list[i + 3])
         i += 4
